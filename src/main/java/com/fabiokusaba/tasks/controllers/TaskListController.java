c5feea33350a96bab081631ee825583ff6e97046
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-08 17:43:55 -0300

    adicionado controller da lista de tarefas

---
+++ src/main/java/com/fabiokusaba/tasks/controllers/TaskListController.java
@@ -0,0 +1,31 @@
+package com.fabiokusaba.tasks.controllers;
+
+import com.fabiokusaba.tasks.domain.dtos.TaskListDto;
+import com.fabiokusaba.tasks.mappers.TaskListMapper;
+import com.fabiokusaba.tasks.services.TaskListService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/task-lists")
+public class TaskListController {
+
+    private final TaskListService taskListService;
+    private final TaskListMapper taskListMapper;
+
+    public TaskListController(TaskListService taskListService, TaskListMapper taskListMapper) {
+        this.taskListService = taskListService;
+        this.taskListMapper = taskListMapper;
+    }
+
+    @GetMapping
+    public List<TaskListDto> listTaskLists() {
+        return taskListService.listTaskLists()
+                .stream()
+                .map(taskListMapper::toDto)
+                .toList();
+    }
+}
