9a4db2015d29e1b9b945dc548cb221d44ffcbd15
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-08 20:06:41 -0300

    adicionado método de remover uma tarefa

--- src/main/java/com/fabiokusaba/tasks/repositories/TaskRepository.java
+++ src/main/java/com/fabiokusaba/tasks/repositories/TaskRepository.java
@@ -12,6 +12,6 @@ import java.util.UUID;
 public interface TaskRepository extends JpaRepository<Task, UUID> {
 
     List<Task> findByTaskListId(UUID taskListId);
-
     Optional<Task> findByTaskListIdAndId(UUID taskListId, UUID id);
+    void deleteByTaskListIdAndId(UUID taskListId, UUID id);
 }
