d916acc3372d0285790472d12110e9e14f2cb7cf
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-08 18:29:14 -0300

    adicionado método de remover uma lista de tarefas

--- src/main/java/com/fabiokusaba/tasks/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/fabiokusaba/tasks/services/impl/TaskListServiceImpl.java
@@ -71,4 +71,9 @@ public class TaskListServiceImpl implements TaskListService {
 
         return taskListRepository.save(existingTaskList);
     }
+
+    @Override
+    public void deleteTaskList(UUID taskListId) {
+        taskListRepository.deleteById(taskListId);
+    }
 }
