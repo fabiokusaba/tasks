a6dd00a1b5d808b2c77040800ac99de7021bb8d9
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-08 17:56:09 -0300

    adicionado endpoint de criar uma lista de tarefas

--- src/main/java/com/fabiokusaba/tasks/controllers/TaskListController.java
+++ src/main/java/com/fabiokusaba/tasks/controllers/TaskListController.java
@@ -1,11 +1,10 @@
 package com.fabiokusaba.tasks.controllers;
 
 import com.fabiokusaba.tasks.domain.dtos.TaskListDto;
+import com.fabiokusaba.tasks.domain.entities.TaskList;
 import com.fabiokusaba.tasks.mappers.TaskListMapper;
 import com.fabiokusaba.tasks.services.TaskListService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +26,11 @@ public class TaskListController {
                 .stream()
                 .map(taskListMapper::toDto)
                 .toList();
+    }
+
+    @PostMapping
+    public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto) {
+        TaskList taskList = taskListService.createTaskList(taskListMapper.fromDto(taskListDto));
+        return taskListMapper.toDto(taskList);
     }
 }
