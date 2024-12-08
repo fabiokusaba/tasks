29c7acacd9a77ace585495e368c6f70a93ca7e7c
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-08 18:08:18 -0300

    adicionado método de buscar uma lista de tarefas pelo ID

--- src/main/java/com/fabiokusaba/tasks/services/TaskListService.java
+++ src/main/java/com/fabiokusaba/tasks/services/TaskListService.java
@@ -3,9 +3,12 @@ package com.fabiokusaba.tasks.services;
 import com.fabiokusaba.tasks.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
 
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskList(UUID id);
 }
