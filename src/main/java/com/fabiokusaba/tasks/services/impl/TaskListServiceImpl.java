b0d5f7a23e756db867bb2380e868e61ac50c9c84
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-08 17:51:14 -0300

    adicionado método de criar uma lista de tarefas

--- src/main/java/com/fabiokusaba/tasks/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/fabiokusaba/tasks/services/impl/TaskListServiceImpl.java
@@ -5,6 +5,7 @@ import com.fabiokusaba.tasks.repositories.TaskListRepository;
 import com.fabiokusaba.tasks.services.TaskListService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -19,5 +20,27 @@ public class TaskListServiceImpl implements TaskListService {
     @Override
     public List<TaskList> listTaskLists() {
         return taskListRepository.findAll();
+    }
+
+    @Override
+    public TaskList createTaskList(TaskList taskList) {
+        if (null != taskList.getId()) {
+            throw new IllegalArgumentException("Task list already has an ID!");
+        }
+
+        if (null == taskList.getTitle() || taskList.getTitle().isBlank()) {
+            throw new IllegalArgumentException("Task list title must be present!");
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+
+        return taskListRepository.save(new TaskList(
+                null,
+                taskList.getTitle(),
+                taskList.getDescription(),
+                null,
+                now,
+                now
+        ));
     }
 }
