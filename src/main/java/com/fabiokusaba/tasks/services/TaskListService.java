d10be7a6c77553f3e9256d78aa9eeba7c84f2da8
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-08 18:19:12 -0300

    adicionado método de atualizar uma lista de tarefas

--- src/main/java/com/fabiokusaba/tasks/services/TaskListService.java
+++ src/main/java/com/fabiokusaba/tasks/services/TaskListService.java
@@ -11,4 +11,5 @@ public interface TaskListService {
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList);
     Optional<TaskList> getTaskList(UUID id);
+    TaskList updateTaskList(UUID taskListId, TaskList taskList);
 }
